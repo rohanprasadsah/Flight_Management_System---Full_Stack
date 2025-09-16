0df6670ff400b8a4bd012d097cb8466a6120d88d
Rohan Prasad Sah <rohanprasadsah70@gmail.com>
2025-09-17 00:31:40 +0530

    Delete Frontend directory

--- Frontend/src/Utils/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import addFlightSlice from './addFlightSlice';
-
-const store = configureStore({
-    reducer: {
-        addFlight: addFlightSlice
-    }
-});
-export default store;
